65bd7cde24b958dbdc9cc88169bbd8e0aed732dc
byokom <bryce_y_2000@hotmail.com>
2023-04-18 19:55:52 -0500

    Fixed stuff in wrong folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,37 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header.js';
-import Search from './components/Search.js';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.js";
+import Search from "./components/Search.js";
 
-
+const UNSPASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-  const [word, setWord] = useState('')
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word)
-  }
-  console.log(word)
+    console.log(word);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setWord("");
+  };
+
   return (
     <div>
       <Header title="Images Gallery 2" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
-
+};
 
 export default App;
