6f8d644448ce0731554aa2a12f27a736fc48a190
byokom <bryce_y_2000@hotmail.com>
2023-07-17 21:11:26 -0500

    sample flask app

---
+++ api/app.py
@@ -0,0 +1,16 @@
+# save this as app.py
+#Terminal: flask run
+
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5050)
+    
