96e194981d71bcf4cbdd9a6812839f2b3c5b9aab
byokom <bryce_y_2000@hotmail.com>
2023-08-28 20:39:19 -0500

    API Key to env

--- api/app.py
+++ api/app.py
@@ -1,15 +1,37 @@
 # save this as app.py
 #Terminal: flask run
+import os 
+import requests
+from flask import Flask, request
+from dotenv import dotenv_values
 
-from flask import Flask
+config = {
+    **dotenv_values(".env.local"),  # load shared development variables
+    **os.environ,  # override loaded values with environment variables
+}
 
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = config.get("UNSPLASH_KEY", "")
 
+if not UNSPLASH_KEY: 
+    raise EnvironmentError("Please create .env.local file and UNSPLASH_KEY variable")
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    #retreive query 
+    word = request.args.get("query") 
+    headers = {
+        "Accept-Version": "v1",
+        "Authorization": "Client-ID "+UNSPLASH_KEY
+    }
+    params = {
+        "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL, headers = headers, params = params )
+    data = response.json()
+    return data
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0", port=5050)
